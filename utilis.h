f39b1a60288819b2d3f8179bdc769b14a577f150
Sydney Petrehn <48782717+Strehn@users.noreply.github.com>
2025-04-06 16:50:20 -0600

    Create utilis.h

---
+++ utilis.h
@@ -0,0 +1,9 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <stdbool.h>
+
+void initialize_grid_random(bool **grid, int width, int height);
+void initialize_grid_static(bool **grid, int width, int height);
+
+#endif // UTILS_H
